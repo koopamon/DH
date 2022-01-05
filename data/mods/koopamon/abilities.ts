0e07e4e14108ff950dff3ed704904e01c02b23e4
RyanDCB2 <79123792+RyanDCB2@users.noreply.github.com>
2022-01-05 00:15:25 -0500

    Update abilities.ts

--- data/mods/koopamon/abilities.ts
+++ data/mods/koopamon/abilities.ts
@@ -329,17 +329,23 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 		id: "poisonleech",
 		name: "Poison Leech",
 		shortDesc: "Heals the user based on Poison/Toxic damage the foe endures.",
+		onStart(target, source, sourceEffect) {
+			this.effectData.stage = 0;
+		},
 		onResidualOrder: 26,
 		onResidualSubOrder: 1,
 		onResidual(pokemon) {
             if (!pokemon.hp) return;
+			if (this.effectData.stage < 15) {
+				this.effectData.stage++;
+			}
             for (const target of pokemon.side.foe.active) {
                 if (!target || !target.hp) continue;
                 if (target.status === 'psn') {
                     this.heal(target.baseMaxhp / 8);
                 }
                 else if (target.status === "tox") {
-                    this.heal((target.baseMaxhp / 16) * target.effectData.stage)
+                    this.heal((target.baseMaxhp / 16) * this.effectData.stage)
                 }
             }
         },
