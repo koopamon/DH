e53b32ca8d64f9e4b9938bd3172089ef8bcec451
RyanDCB2 <79123792+RyanDCB2@users.noreply.github.com>
2022-01-13 23:56:18 -0500

    Update items.ts

--- data/items.ts
+++ data/items.ts
@@ -3112,6 +3112,26 @@ export const Items: {[itemid: string]: ItemData} = {
 		num: 215,
 		gen: 3,
 	},
+	magicalcapsule: {
+		name: "Magical Capsule",
+		shortDesc: "If the holder's species can evolve, its Attack and Sp. Atk are 1.5x.",
+		spritenum: 130,
+		fling: {
+			basePower: 40,
+		},
+		onModifyDefPriority: 2,
+		onModifyDef(atk, pokemon) {
+			if (pokemon.baseSpecies.nfe) {
+				return this.chainModify(1.5);
+			}
+		},
+		onModifySpDPriority: 2,
+		onModifySpD(spa, pokemon) {
+			if (pokemon.baseSpecies.nfe) {
+				return this.chainModify(1.5);
+			}
+		},
+	},
 	magmarizer: {
 		name: "Magmarizer",
 		spritenum: 272,
